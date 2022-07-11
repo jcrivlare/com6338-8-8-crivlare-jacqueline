89e6f85a0f6fd5c932b53d8256e55d36831eac64
Jackie Crivlare <jcrivlare@ufl.edu>
2022-07-10 18:34:25 -0700

    editing

--- index.js
+++ index.js
@@ -1,23 +1,100 @@
-var weather = document.getElementById("weather");
-var form = document.querySelector("form");
-var input = document.getElementById("weather-search");
-var api_key = "8ecac9ebfb54144eef31588a12e23a03";
-//var location = "";
+const api_key = "8ecac9ebfb54144eef31588a12e23a03";
+const form = document.querySelector("form");
+const section = document.getElementById("weather");
+const search = document.getElementById("weather-search");
 
 form.onsubmit = async (e) => {
   e.preventDefault();
-  var query = input.value;
+  let query = search.value;
   let location = document.createElement("h2");
-  console.log("outside fetch: ", weather);
+  search.value = "";
+  section.innerHTML = "";
+  // this.ClearForm();
+
   var URL = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=imperial&appid=${api_key}`;
   fetch(URL).then((response) => {
-    //console.log(response);
+    console.log(response);
     response.json().then((data) => {
       console.log("data", data);
-      location = data.name + ", " + data.sys.country;
-      console.log("location", location);
+
+      /* In Case Of Error */
+      if (data.cod === "404" || data.cod === "400" || data.data?.cod === 404) {
+        console.log("404 code");
+        let h2 = document.createElement("h2");
+        h2.innerHTML = "Location Not Found";
+        section.replaceChildren(h2);
+        return;
+      } else {
+        /* In Case Of Success */
+        location = data.name + ", " + data.sys.country;
+
+        let city = createElement("h2", { innerHTML: location });
+        let mapLink = createElement("a", {
+          innerHTML: "Click to view map",
+          href: `https://www.google.com/maps/search/?api=1&query=${data.coord.lat},${data.coord.lon}`,
+          target: "__BLANK",
+        });
+
+        let icon = createElement("img", {
+          src: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+        });
+
+        let condition = createElement(
+          "p",
+          {
+            innerHTML: data.weather[0].description,
+          },
+          {
+            textTransform: "capitalize",
+          }
+        );
+
+        let current = createElement("p", {
+          innerHTML: `Current: ${data.main.temp}&deg;`,
+        });
+
+        let feelsLike = createElement("p", {
+          innerHTML: `Feels like: ${data.main.feels_like}&deg;`,
+        });
+
+        let date = new Date(data.dt * 1000);
+
+        let lastUpdated = createElement("p", {
+          innerHTML: `Last updated: ${date.toLocaleTimeString("en-US", {
+            hour: "numeric",
+            minute: "2-digit",
+          })}`,
+        });
+
+        section.replaceChildren(
+          city,
+          mapLink,
+          icon,
+          condition,
+          current,
+          feelsLike,
+          lastUpdated
+        );
+      }
     });
   });
-  //city.innerHTML = `${query}`;
-  //console.log("city", city);
 };
+
+const createElement = (el, props, styles = null) => {
+  let element = document.createElement(el);
+  for (let prop in props) {
+    element[prop] = props[prop];
+  }
+  if (styles) {
+    for (let style in styles) {
+      element.style[style] = styles[style];
+    }
+  }
+  return element;
+};
+
+function ClearForm() {
+  console.log("ClearForm");
+  search.value = "";
+  section.innerHTML = "";
+}
